a9f7b490ca67f90b90a976f332f3bef7742f2634
João Matos <joaomatos19@gmail.com>
2022-03-18 10:57:00 +0000

    Added Coin and Spike Prefab

---
+++ Assets/Game/Scripts/Mechanics/Spike.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            damageable.TakeDamage(1);
+        }
+    }
+}
